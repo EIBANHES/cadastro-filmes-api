6eb9d77465f53750b1543d58190f2a70b165e679
Emanuel Ibanhes <emanuel.ibanhes.contato@gmail.com>
2024-04-16 17:49:15 -0300

    Commit changes

---
+++ src/server.js
@@ -0,0 +1,29 @@
+//Inicialização do express
+const express = require('express');
+const app = express();
+
+// Import de rotas
+const routes = require("./routes")
+app.use(routes);
+
+// Tratativa de erros
+require('express-async-errors')
+const AppError = require("./utils/AppError")
+
+app.use((error, request, response, next) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message
+    });
+  }
+  console.error(error);
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error"
+  })
+});
+
+// Porta da aplicação
+const PORT = 3333;
+app.listen(PORT, () => console.log(`API rodando na porta ${PORT}!`));
