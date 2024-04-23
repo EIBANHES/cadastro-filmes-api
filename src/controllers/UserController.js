8b198677a2955291abfe5e71b5ad2818b3015660
Emanuel Ibanhes <emanuel.ibanhes.contato@gmail.com>
2024-04-22 21:13:53 -0300

    cadastro de filmes, para continuar o desenvolvimento

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,7 +1,6 @@
 const knex = require("../database/knex");
 const { hash, compare } = require("bcryptjs")
 const AppError = require("../utils/AppError");
-const usersRoutes = require("../routes/users.routes");
 
 class UserController {
 
@@ -11,9 +10,7 @@ class UserController {
       const userExist = await knex('users').where({ email }).first();
 
       if (userExist) {
-        return response.json({
-          message: "Usuário já existente"
-        });
+        throw new AppError("Usuário já existente!");
       }
 
       const passwordHash = await hash(password, 8);
@@ -27,9 +24,9 @@ class UserController {
       response.json({});
 
     } catch (error) {
-      return response.json({
-        message: "Erro ao cadastrar usuário!"
-      })
+      return response.status(error.statusCode).json({
+        message: error.message
+      });
     }
   }
 
@@ -49,49 +46,7 @@ class UserController {
   }
 
   async updateUser(request, response) {
-    const { user_id } = request.params;
     const { name, email, old_password, password } = request.body;
-
-    // Verificar se o usuário existe
-    const user = await knex("users").where({ id: user_id }).first();
-    if (!user) {
-      return response.json({
-        message: "Usuário não encontrado"
-      });
-    }
-
-    // Verificar se o novo email já existe para outro usuário
-    const emailExist = await knex('users').where({ email }).first();
-    if (emailExist) {
-      return response.json({
-        message: "Email já existente"
-      });
-    }
-
-    // Verificar se a senha antiga está correta
-    if (old_password) {
-      const verifyPass = await compare(old_password, user.password);
-      if (!verifyPass) {
-        return response.json({
-          message: "Senha antiga incorreta"
-        });
-      }
-    }
-
-    // Atualizar os dados do usuário
-    const updatedUser = {};
-    updatedUser.name = name ?? user.name;
-    updatedUser.email = email ?? user.email;
-    if (password) {
-      updatedUser.password = await hash(password, 8) ?? user.password;
-    }
-
-    await knex("users").where({ id: user_id }).update(updatedUser);
-
-    return response.json({
-      message: "Usuário atualizado com sucesso",
-      user: updatedUser
-    });
   }
 
 
